9c73a91ae35ac78bf0a7d28c4849b66801ccb321
nihaar-c <charagulla.nihaar@gmail.com>
2023-11-24 01:24:17 -0800

    Updated API endpoints & business logic

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request
 from dotenv import load_dotenv
 from pymongo import MongoClient
 from objects import Product, SalesPerson, Customer, Sales, Discount
+from bson import ObjectId
 import os
 
 #access Mongo connection string from .env
@@ -19,31 +20,49 @@ result = collection.insert_one(test_doc)
 print (f"Test doc inteseted with _id: {result.inserted_id}")
 retrieved_doc = collection.find_one({"name": "Test"})
 print(f"Retrieved doc: {retrieved_doc}")
-'''
 
+test_doc = {
+	"product": "iPhone",
+	"salesperson": "Mike",
+	"customer": "Nihaar",
+	"date": "11/22/23"
+}
+result = db['Sales'].insert_one(test_doc)
+print (f"Test doc inteseted with _id: {result.inserted_id}")
+'''
 
 app = Flask(__name__)
 
 #implement api endpoints for each route
 
+
+#convert objectid to string
+def bsonToJson(data):
+    for doc in data:
+        doc["_id"] = str(doc["_id"])
+    return data
+
 #products
 @app.route('/products', methods=['GET', 'POST'])
 def manageProducts():
     if request.method == "GET":
-        products = db.products.find()
-        return jsonify([product for product in products])
+        productsCursor = db['Products'].find()
+        products = list(productsCursor)
+        return jsonify(bsonToJson(products))
     elif request.method == "POST":
         productData = request.json
+        print(f"{productData}")
         product = Product(**productData)
-        db.products.insert_one(product.to_json())
+        db['Products'].insert_one(product.to_json())
         return jsonify(product.to_json()), 201
 
 #Salesperson
 @app.route('/salesperson', methods=['GET', 'POST'])
 def manageSalesPeople():
     if request.method == "GET":
-        salesPerson = db.salesPerson.find()
-        return jsonify([person for person in salesPerson])
+        salesPersonCursor = db['Salesperson'].find()
+        salesPerson = list(salesPersonCursor)
+        return jsonify(bsonToJson(salesPerson))
     elif request.method == "POST":
         peopleData = request.json
         person = SalesPerson(**peopleData)
@@ -53,8 +72,9 @@ def manageSalesPeople():
 @app.route('/customer', methods=['GET', 'POST'])
 def manageCustomers():
     if request.method == "GET":
-        customers = db.customers.find()
-        return jsonify([customer for customer in customers])
+        customersCursor = db['Customers'].find()
+        customers = list(customersCursor)
+        return jsonify(bsonToJson(customers))
     elif request.method == "POST":
         customerData = request.json
         customer = Customer(**customerData)
@@ -64,26 +84,66 @@ def manageCustomers():
 @app.route('/sales', methods=['GET', 'POST'])
 def manageSales():
     if request.method == "GET":
-        sales = db.sales.find()
-        return jsonify([sale for sale in sales])
-
+        salesCursor = db['Sales'].find()
+        sales = list(salesCursor)
+        return jsonify(bsonToJson(sales))
     elif request.method == "POST":
         saleData = request.json
-        sale = Product(**saleData)
+        sale = Sales(**saleData)
         db.sales.insert_one(sale.to_json())
         return jsonify(sale.to_json()), 201
 #Discount
 @app.route('/discount', methods=['GET', 'POST'])
 def manageDiscounts():
     if request.method == "GET":
-        discounts = db.discounts.find()
-        return jsonify([discount for discount in discounts])
+        discountsCursor = db['Discounts'].find()
+        discounts = list(discountsCursor)
+        return jsonify(bsonToJson(discounts))
 
     elif request.method == "POST":
         discountData = request.json
-        discount = Product(**discountData)
+        discount = Discount(**discountData)
         db.discounts.insert_one(discount.to_json())
         return jsonify(discount.to_json()), 201
+
+#business logic -- commissions & bonuses
+
+#comission by sale
+def calculateCommission(product):
+    product = db['Products'].find({"Name": product})
+    product = list(product)
+    print (product)
+    return (product[0]["Commission"] * product[0]["Sale Price"] if product else 0)
+
+@app.route('/commissions', methods=['POST'])
+def getCommission():
+    sale = request.json
+    sale = Sales(**sale)
+    commission = calculateCommission(sale.product)
+    return jsonify({"Commission": commission}), 200
+
+#bonus
+#salesperson to find bonus of
+def calculateBonus(sales, person):
+    totalCommission = 0
+    for i in sales:
+        if person == i['salesperson']:
+            totalCommission += calculateCommission(i)
+    
+    return int(0.5 * totalCommission)
+#not working -- need to investigate more to fix
+@app.route('/bonus/<personID>', methods=['GET'])
+def getBonus(personID):
+    #sales = db.sales.find({"Sales Person": personID})
+    #sales = list(sales)
+    #del sales[0]['_id']
+    #print(sales[0])
+    sales = db['Sales'].find()
+    sales = list(sales)
+    print(sales)
+
+    calculateBonus(sales, personID)
+    #return jsonify({"bonus": bonus}), 200        
 
 
 def home():
--- backend/objects.py
+++ backend/objects.py
@@ -5,25 +5,37 @@
 
 #product
 class Product:
-    def __init__(self, name, manufacturer, style, purchasePrice, salePrice, currentAmt, comission):
+    def __init__(self, name, manufacturer, style, purchasePrice, salePrice, currentAmt, commission):
         self.name = name
         self.manufacturer = manufacturer
         self.style = style
         self.purchasePrice = purchasePrice
         self.salePrice = salePrice
         self.currentAmt = currentAmt
-        self.comission = comission
+        self.commission = commission
     
     def to_json(self):
         return {
-            "name": self.name,
-            "manufacturer": self.manufacturer,
-            "style": self.style,
-            "purchasePrice": self.purchasePrice,
-            "salePrice": self.salePrice,
-            "currentAmt": self.currentAmt,
-            "comission": self.comission
+            "Name": self.name,
+            "Manufacturer": self.manufacturer,
+            "Style": self.style,
+            "Purchase Price": self.purchasePrice,
+            "Sale Price": self.salePrice,
+            "Current Amount": self.currentAmt,
+            "Commission": self.commission
         }
+'''
+api request test
+{
+	"name": "iPhone",
+	"manufacturer": "Apple",
+  "style": "Red",
+  "purchasePrice": 200,
+  "salePrice": 500,
+  "currentAmt": 100,
+  "commission": 0.2
+}
+'''
 
 #salesperson
 class SalesPerson:
@@ -79,6 +91,13 @@ class Sales:
             "Customer": self.customer,
             "Date": self.date
         }
+'''
+{
+	"product": "iPhone",
+	"salesperson": "Mike",
+	"customer": "Nihaar",
+	"date": "11/23/23"
+}'''
 #discount
 class Discount:
     def __init__(self, product, startDate, endDate, amt):
